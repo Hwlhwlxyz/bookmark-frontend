b165580a8ab35b2b5e76cff76c5b5a356b89dea6
Hwlhwlxyz <huweilun1994@hotmail.com>
2023-05-29 17:35:57 -0400

    use crypto-js to support chinese

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,3 +1,28 @@
+import Crypto from 'crypto-js';
+
+export function base64encode(input: string) {
+    // INIT
+    const myString = '75322541'; // Utf8-encoded string
+    // PROCESS
+    const encodedWord = Crypto.enc.Utf8.parse(input); // encodedWord Array object
+    const encoded = Crypto.enc.Base64.stringify(encodedWord); // string: 'NzUzMjI1NDE='
+    return encoded;
+}
+
+export function base64decode(input: string) {
+    let decoded = '';
+    try {
+        const encodedWord = Crypto.enc.Base64.parse(input); // encodedWord via Base64.parse()
+        decoded = Crypto.enc.Utf8.stringify(encodedWord); // decode encodedWord via Utf8.stringify() '75322541'
+
+    }
+    catch (e) {
+        console.log("base64decode error", e);
+        decoded = 'cannot decode';
+    }
+    return decoded;
+}
+
 
 export function isSessionError(err: any) {
     switch (err.toString().replace(/\(\d+\)/g, "").trim().toLowerCase()) {
@@ -14,13 +39,13 @@ export function isBrowser() {
 }
 
 export function parseObjToUrl(obj: Object) {
-    return window.btoa(JSON.stringify(obj));
+    return base64encode(JSON.stringify(obj));
 }
 
 export function parseObjFromUrl(urlstr: string) {
     let jsonstr;
     try {
-        jsonstr = window.atob(urlstr)
+        jsonstr = base64decode(urlstr)
         return JSON.parse(jsonstr);
     }
     catch (e) {
