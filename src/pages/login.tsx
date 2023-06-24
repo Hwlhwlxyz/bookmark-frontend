7b77fe0da43254b2f3dabfb1f3c7dd0d404768e0
Hwlhwlxyz <huweilun1994@hotmail.com>
2023-06-23 23:31:09 -0400

    fix == bug

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -104,7 +104,7 @@ export default function Login() {
     };
 
     function comfirmLogin(e: KeyboardEvent<HTMLInputElement>): void {
-        if ((e.key = 'Enter')) {
+        if (e.key == 'Enter') {
             handleSubmit(undefined);
         }
     }
